c795606118aefb341056976cf8bdc7407685819f
laraferreira05 <lara.lisandra@ges.inatel.br>
2026-08-25 08:47:14 -0300

    exercicio 2
    Add user input functionality to count occurrences of a number in a list.

--- Relatório 02/Exercício 02.lua
+++ Relatório 02/Exercício 02.lua
@@ -1,1 +1,24 @@
+print("Digite a quantidade de elementos (N):")
+local N = tonumber(io.read())
+local numeros = {}
 
+
+for i = 1, N do
+    print("Digite o elemento " .. i .. ":")
+    local quantidade = tonumber(io.read())
+    table.insert(numeros, quantidade)
+end
+
+print("Digite o numero X a ser buscado:")
+local X = tonumber(io.read())
+
+local cont = 0
+
+for i = 1, #numeros do
+    if numeros[i] == X then
+        cont = cont + 1
+    end
+end
+
+print("Saida esperada:")
+print("O numero " .. X .. " aparece " .. cont .. " vez(es) na tabela")
