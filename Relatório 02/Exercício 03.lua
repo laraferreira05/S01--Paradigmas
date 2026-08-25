095cd1f8a511951dc9aa4e7980f5ec2ca6fd64b2
laraferreira05 <lara.lisandra@ges.inatel.br>
2026-08-25 09:33:00 -0300

    exercicio 3
    Added functionality to filter and display elements greater than a specified limit.

--- Relatório 02/Exercício 03.lua
+++ Relatório 02/Exercício 03.lua
@@ -1,1 +1,33 @@
+print("Digite a quantidade de elementos(N):")
+local N = tonumber(io.read())
+local tabela = {}
 
+
+for i = 1, N do
+    print("Digite o elemento " .. i .. ":")
+    local quantidade = tonumber(io.read())
+    table.insert(tabela, quantidade)
+end
+
+print("Digite o valor limite (K):")
+local limite = tonumber(io.read())
+local cont = 0
+
+function filtrarMaiores(tabela, limite)
+
+    local maior = {}
+    for i = 1, #tabela do
+        if tabela[i] > limite then
+            table.insert(maior, tabela[i]) 
+            cont = cont + 1
+        end
+    end        
+    return maior
+end
+
+local resultado = filtrarMaiores(tabela,limite)
+print("---Elementos maiores que " .. limite .. " ---")
+
+    for i = 1, #resultado do
+    print(resultado[i])
+end
