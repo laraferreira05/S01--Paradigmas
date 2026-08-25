5b786ed230c5670da11a0432381cb874efb18535
laraferreira05 <lara.lisandra@ges.inatel.br>
2026-08-25 09:36:50 -0300

    adicionei a função que havia esquecido

--- Relatório 02/Exercício 02.lua
+++ Relatório 02/Exercício 02.lua
@@ -12,12 +12,15 @@ end
 print("Digite o numero X a ser buscado:")
 local X = tonumber(io.read())
 
+function contarOcorrencias(numeros, X)
 local cont = 0
-
 for i = 1, #numeros do
     if numeros[i] == X then
         cont = cont + 1
     end
+ end
+ return cont
 end
 
-print("O numero " .. X .. " aparece " .. cont .. " vez(es) na tabela")
+ local resultado = contarOcorrencias(numeros,X)
+print("O numero " .. X .. " aparece " .. resultado .. " vez(es) na tabela")
