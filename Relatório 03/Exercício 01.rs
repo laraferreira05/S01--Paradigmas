bf3c8e2b22b7670b904664b4e4b8a8d5e2563fa6
laraferreira05 <lara.lisandra@ges.inatel.br>
2026-09-01 10:09:09 -0300

    execução do exercício 1

--- Relatório 03/Exercício 01.rs
+++ Relatório 03/Exercício 01.rs
@@ -1,1 +1,53 @@
+use std::io::{self, Write};
 
+fn validar_placa(placa: &str) -> bool {
+
+    if placa.len() < 7 {
+        return false;
+    }
+
+    let qtd_maiusculas = placa
+        .chars()
+        .filter(|c| c.is_ascii_uppercase())
+        .count();
+
+
+    if qtd_maiusculas < 4 {
+        return false;
+    }
+
+    let qtd_numeros = placa
+        .chars()
+        .filter(|c| c.is_numeric())
+        .count();
+
+    if qtd_numeros < 2 {
+        return false;
+    }
+
+    true
+}
+
+fn main() {
+    loop {
+        print!("Digite a placa do veiculo: ");
+        io::stdout()
+            .flush()
+            .expect("Falha ao descarregar a tela");
+
+        let mut entrada = String::new();
+
+        io::stdin()
+            .read_line(&mut entrada)
+            .expect("Falha ao ler a entrada");
+
+        let placa_limpa = entrada.trim();
+
+        if validar_placa(placa_limpa) {
+            println!("Placa cadastrada no sistema!");
+            break;
+        } else {
+            println!("Placa invalida. Tente novamente!\n");
+        }
+    }
+}
